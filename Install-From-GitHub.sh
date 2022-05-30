6d1ea796fbdd41bbfeb302df5f13aad46619c6b7
Bill Wetzel <bill@wrwetzel.com>
2022-05-30 11:25:21 -0400

    Restructure installation scripts, add Install.sh

--- Install-From-GitHub.sh
+++ Install-From-GitHub.sh
@@ -6,57 +6,7 @@
 #   Run this in the directory where found.
 #   Mimics a pip install in .local
 #   Taken from Install-From-Tar.sh, of which it is nearly identical.
-#   Start-From-Tar.sh is same for this and not changed.
 
 # --------------------------------------------------------------------------------------
 
-if [[ ! -d ~/.local ]]
-then
-    echo "Creating ~/.local"
-    mkdir ~/.local
-fi
-
-if [[ ! -d ~/.local/bin ]]
-then
-    echo "Creating ~/.local/bin"
-    mkdir ~/.local/bin
-fi
-
-if [[ ! -d ~/.local/share ]]
-then
-    echo "Creating ~/.local/share"
-    mkdir ~/.local/share
-fi
-
-if [[ ! -d ~/.local/share/applications ]]
-then
-    echo "Creating ~/.local/share/applications"
-    mkdir ~/.local/share/applications
-fi
-
-if [[ ! -d ~/.local/share/birdland ]]
-then
-    echo "Creating ~/.local/share/birdland"
-    mkdir ~/.local/share/birdland
-fi
-
-cp -r src/*                                                     ~/.local/share/birdland
-ln -sf ~/.local/share/birdland/birdland/Start-From-Tar.sh       ~/.local/bin/birdland
-ln -sf ~/.local/share/birdland/birdland/Start-From-Tar.sh       ~/.local/bin/bl-build-tables
-ln -sf ~/.local/share/birdland/birdland/Start-From-Tar.sh       ~/.local/bin/bl-diff-index
-
-ln -sf ~/.local/share/birdland/birdland/build_tables.py         ~/.local/bin/bl-build-tables
-ln -sf ~/.local/share/birdland/birdland/diff_index.py           ~/.local/bin/bl-diff-index
-# cp src/birdland/birdland-tar.desktop                          ~/.local/share/applications/birdland.desktop
-# sed "s,~,$HOME,g" src/birdland/birdland-tar.desktop >         ~/.local/share/applications/birdland.desktop
-
-cp Package_Type_GitHub.txt                                       ~/.local/share/birdland/birdland
-# cp Start-From-GitHub.sh                                          ~/.local/share/birdland/birdland
-
-echo "Copied Birdland data folders to: '~/.local/share/birdland'"
-echo "Linked executables in '~/.local/share/birdland/birdland' to '~/.local/bin'"
-echo "Be sure '~/.local/bin' is in your PATH"
-echo "Birdland executable is: 'birdland'"
-echo "For more information see: './ReadMe-GitHub.md'"
-echo "Remove Birdland with: '~/.local/share/birdland/birdland/Remove-Birdland.sh'"
-echo "Installation complete. '../Birdland' or '../Birdland-main' are not needed for running Birdland and may be removed."
+./Install.sh GitHub
---
+++ Install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------------
+#   Install.sh - Install Birdland with Package_Type given on command line.
+#   Called from Install-From-Tar.sh or Install-From-GitHub.sh.
+#   WRW 30 May 2022
+# --------------------------------------------------------------------------------------
+
+command="$1"
+case "$command" in
+    "Tar")
+        Package_Type=Package_Type_Tar.txt
+    ;;
+
+    "GitHub")
+        Package_Type=Package_Type_GitHub.txt
+    ;;
+
+    *)
+        echo "Usage: $0 [Tar|GitHub]"
+        exit 1
+
+esac
+
+if [[ ! -d ~/.local ]]
+then
+    echo "Creating ~/.local"
+    mkdir ~/.local
+fi
+
+if [[ ! -d ~/.local/bin ]]
+then
+    echo "Creating ~/.local/bin"
+    mkdir ~/.local/bin
+fi
+
+if [[ ! -d ~/.local/share ]]
+then
+    echo "Creating ~/.local/share"
+    mkdir ~/.local/share
+fi
+
+if [[ ! -d ~/.local/share/applications ]]
+then
+    echo "Creating ~/.local/share/applications"
+    mkdir ~/.local/share/applications
+fi
+
+if [[ ! -d ~/.local/share/birdland ]]
+then
+    echo "Creating ~/.local/share/birdland"
+    mkdir ~/.local/share/birdland
+fi
+
+cp -r src/*                                                     ~/.local/share/birdland
+
+ln -sf ~/.local/share/birdland/birdland/birdland.py             ~/.local/bin/birdland
+ln -sf ~/.local/share/birdland/birdland/build_tables.py         ~/.local/bin/bl-build-tables
+ln -sf ~/.local/share/birdland/birdland/diff_index.py           ~/.local/bin/bl-diff-index
+
+cp $Package_Type                                                ~/.local/share/birdland/birdland
+
+echo "Copied Birdland data folders to: '~/.local/share/birdland'"
+echo "Linked executables in '~/.local/share/birdland/birdland' to '~/.local/bin'"
+echo "Be sure '~/.local/bin' is in your PATH"
+echo "Birdland executable is: 'birdland'"
+echo "For more information see: './ReadMe-Tar.md'"
+echo "Remove Birdland with: '~/.local/share/birdland/birdland/Remove-Birdland.sh'"
+echo "Installation complete. '../Birdland' or '../Birdland-main' are not needed for running Birdland and may be removed."
